1322b2c983f8e2a6e950e0dca79b1ccc12cb6f60
Jesse Smart <jsmartdev@protonmail.com>
2021-05-15 22:04:36 -0400

    starting over on getNum function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 
+
 /* Template Functions */
 
 function introTemplate() {
@@ -38,12 +39,13 @@ function formTemplate() {
 
 function responseTemplate() {
     return `<div class="text-wrap">
-                <p>Behold, your secret magical number is ${magic.value}.</p>
+                <p>Behold, your secret magical number is ${magic}.</p>
             </div>
             <form id="return" class="btn-wrap">
                 <input class="button" type="submit" value="Return">
             </form>`;
 }
+
 
 /* Render Functions */
 
@@ -60,72 +62,6 @@ function renderForm() {
 function renderResponse() {
     template = responseTemplate();
     $('main').html(template);
-}
-
-/* Arithmetic Function */
-
-function getNum() {
-    /* declare variables for all form inputs */
-
-    let a = document.getElementById("your-month").value;
-    let b = document.getElementById("your-day").value;
-    let c = document.getElementById("mom-month").value;
-    let d = document.getElementById("mom-day").value;
-    let e = document.getElementById("dad-month").value;
-    let f = document.getElementById("dad-day").value;
-
-    /* if your-month >= your-day, let g = your-month - your-day */
-    /* else let a = your-day - your-month */
-
-    if (a >= b) {
-        let g = a - b;
-    }
-    else {
-        let g = b - a;
-    }
-
-    /* if mom-month >= mom-day, let h = mom-month - mom-day */
-    /* else let b = mom-day - mom-month */
-
-    if (c >= d) {
-        let h = c - d;
-    }
-    else {
-        let h = d - c;
-    }
-
-    /* if dad-month >= dad-day, let i = dad-month - dad-day */
-    /* else let c = dad-day - dad-month */
-
-    if (e >= f) {
-        let i = e - f;
-    }
-    else {
-        let i = f - e;
-    }
-
-    /* if g >= h, let j = g - h */
-    /* else let j = i - h */
-
-    if (h >= i) {
-        let j = h - i;
-    }
-    else {
-        let j = i - h;
-    }
-
-    /* if d >= c, let magic = d - c; */
-    /* else let magic = c - d */
-
-    if (g >= j) {
-        let magic = g - j;
-    }
-    else {
-        let magic = j - g;
-    }
-
-    /* return magic number */
-    return magic;
 }
 
 /* Event Handlers */
@@ -145,6 +81,7 @@ function handleSubmit() {
     })
 }
 
+
 function handleReturn() {
     $('main').on("submit", "#return", function(event) {
         event.preventDefault();
@@ -158,7 +95,7 @@ function main() {
     renderIntro();
     handleBegin();
     handleSubmit();
-    handleBack();
+    handleReturn();
 }
 
 $(main);
